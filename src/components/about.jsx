96a4561b3863998236e759b3694c4c342ebd4062
Maedot <maedotmetsihet4@gmail.com>
2024-09-23 10:39:46 +0300

    changes

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useRef, useState } from "react";
 
 export const About = (props) => {
-  const aboutTextRef = useRef(null);
   const missionItemRef = useRef(null);
   const valueItemRef = useRef(null);
-  const imageRef = useRef(null);
-  const [animateParagraph, setAnimateParagraph] = useState(false);
+  const [animateItems, setAnimateItems] = useState(false);
 
   useEffect(() => {
+    // Check if the screen width is less than or equal to 768px (mobile view)
+    if (window.innerWidth <= 768) {
+      setAnimateItems(true);
+    }
+
     const observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
@@ -21,23 +24,13 @@ export const About = (props) => {
       { threshold: 0.5 }
     );
 
-    const elementsToObserve = [
-      aboutTextRef,
-      missionItemRef,
-      valueItemRef,
-      imageRef,
-    ];
+    const elementsToObserve = [missionItemRef, valueItemRef];
 
     elementsToObserve.forEach(ref => {
       if (ref.current) {
         observer.observe(ref.current);
       }
     });
-
-    // Check if the screen width is less than or equal to 768px (mobile view)
-    if (window.innerWidth <= 768) {
-      setAnimateParagraph(true);
-    }
 
     return () => {
       observer.disconnect();
@@ -49,21 +42,24 @@ export const About = (props) => {
       <div className="container">
         <div className="row">
           <div className="col-12 col-md-6">
-            <div 
-              className={`about-text ${animateParagraph ? 'animate' : ''} animate-from-bottom`} 
-              ref={aboutTextRef}
-            >
+            <div className="about-text">
               <h2>About Us</h2>
               <p>{props.data ? props.data.paragraph : "loading..."}</p>
               <div className="row">
                 <div className="col-12 col-sm-6">
-                  <div className="about-item animate-from-right animate-delay-1" ref={missionItemRef}>
+                  <div 
+                    className={`about-item ${animateItems ? 'animate' : ''} animate-from-right animate-delay-1`} 
+                    ref={missionItemRef}
+                  >
                     <h3>Mission</h3>
                     <p>{props.data ? props.data.mission : "loading..."}</p>
                   </div>
                 </div>
                 <div className="col-12 col-sm-6">
-                  <div className="about-item animate-from-right animate-delay-3" ref={valueItemRef}>
+                  <div 
+                    className={`about-item ${animateItems ? 'animate' : ''} animate-from-right animate-delay-3`} 
+                    ref={valueItemRef}
+                  >
                     <h3>Values</h3>
                     <p>{props.data ? props.data.values : "loading..."}</p>
                   </div>
